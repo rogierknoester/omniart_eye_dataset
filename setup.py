17bd0ee10aca595dab87af27d9f9dbbeadb6f32b
Rogier Knoester <knoesterrogier@gmail.com>
2019-05-13 18:07:19 +0200

    fix setuptools

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='omniart_eye_dataset',
-                 version='0.1',
+                 version='0.1.1',
                  description='A PyTorch dataset of the eyes found in the OmniArt dataset',
                  long_description=long_description,
                  long_description_content_type="text/markdown",
-                 url='http://github.com/rogierkn/omniart_eye_dataset',
+                 url='http://github.com/rogierknoester/omniart_eye_dataset',
                  author='Rogier Knoester',
                  author_email='knoesterrogier+omniart@gmail.com',
                  license='MIT',
